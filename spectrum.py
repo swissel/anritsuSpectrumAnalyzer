a07a7dcc0a4ef157b1fe030d48f59ebaf02f738e
swissel <stephanie.wissel@gmail.com>
2018-07-27 18:26:49 -0700

    first spectrum.py commit

---
+++ spectrum.py
@@ -0,0 +1,16 @@
+import visa
+
+rm = visa.ResourceManager()
+dev_name = "TCPIP0::169.254.24.65::INSTR"
+sa = rm.get_instrument(dev_name)
+# need to wait 3 s for a response from the Anritsu Spectrum Analyzer
+sa.timeout = 3000
+
+# Read the identificaiton string
+stat = sa.write("*IDN?\n")
+read = sa.read()
+print(stat, read)
+
+read = sa.query("*IDN?\n")
+print(read)
+
